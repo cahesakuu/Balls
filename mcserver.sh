eac5c3bb0e19de8a351d912a97f86b6098ad5dce
cahesakuu <cahesaku@proton.me>
2024-12-18 13:14:22 +0600

    Update mcserver.sh

--- mcserver.sh
+++ mcserver.sh
@@ -6,6 +6,7 @@ wget -O server.jar https://api.purpurmc.org/v2/purpur/1.21.1/2329/download/
 java -jar server.jar
 echo eula=true > eula.txt
 touch start.sh 
-echo "cd /minecraft-server/ && java -Xmx1985 -Xms1985 -jar /minecraft-server/server.jar" > start.sh
+echo "java -Xmx1G -Xms1G -jar server.jar" > start.sh
+chmod +x start.sh
 echo "you can now go to /sdcard and then start the start.sh script using bash."
 read -n 1 -s -r -p "Press any key to continue"
